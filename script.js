075ec6d4bb5af91bfd9ebadbfc58d40b659b46a8
Ronan Gleeson <ronangleeson11@gmail.com>
2026-01-03 00:47:08 -0500

    Add listeners to mkae screen update to show button clicked

--- script.js
+++ script.js
@@ -18,23 +18,67 @@ function operate(op1, op2, opr) {
     switch (opr) {
         case "add":
             return add(op1, op2);
-            break;
         case "subtract":
             return subtract(op1, op2);
-            break;
         case "multiply":
             return multiply(op1, op2);
-            break;
         case "divide":
             return divide(op1, op2);
-            break;            
     }
 }
 
 let op1, op2, opr;
-
-buttons = document.querySelector(".buttons");
+screen = document.querySelector("#screen");
+buttons = document.querySelector("#buttons");
 buttons.addEventListener("click", (event) => {
-    console.log(event);
+    console.log(event.target.id);
+    let symbol = "";
+    switch (event.target.id) {
+        case "add":
+            symbol = "+";
+            break;
+        case "subtract":
+            symbol = "-";
+            break;
+        case "multiply":
+            symbol = "*";
+            break;
+        case "divide":
+            symbol = "/";
+            break;
+        case "d1":
+            symbol = "1";
+            break;
+        case "d2":
+            symbol = "2";
+            break;
+        case "d3":
+            symbol = "3";
+            break;
+        case "d4":
+            symbol = "4";
+            break;
+        case "d5":
+            symbol = "5";
+            break;
+        case "d6":
+            symbol = "6";
+            break;
+        case "d7":
+            symbol = "7";
+            break;
+        case "d8":
+            symbol = "8";
+            break;
+        case "d9":
+            symbol = "9 ";
+            break;
+        case "clear":
+            symbol = "";
+            break;
+        case "=":
+            symbol ="";
+            break;    
+    }
+    screen.textContent = symbol;
 });
-screen = document.querySelector(".screen");
